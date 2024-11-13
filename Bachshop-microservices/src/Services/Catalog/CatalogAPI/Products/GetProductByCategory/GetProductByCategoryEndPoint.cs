4dbb25758ca83c92b4c33c2242ae79c631cbfce9
Bashar Saab <bashar.saab@gmail.com>
2024-11-13 17:48:46 -0500

    second day - part 2

---
+++ Bachshop-microservices/src/Services/Catalog/CatalogAPI/Products/GetProductByCategory/GetProductByCategoryEndPoint.cs
@@ -0,0 +1,13 @@
+﻿
+namespace CatalogAPI.Products.GetProductByCategory
+{
+    public record GetProductByCategoryRequest(string Category);
+    public record GetProductByCategoryResponse(IEnumerable<Product> Products);
+    public class GetProductByCategoryEndPoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

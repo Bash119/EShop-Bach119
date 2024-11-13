f4efe1db9d325c63289021de6db6435e33b5d908
Bashar Saab <bashar.saab@gmail.com>
2024-11-13 16:04:11 -0500

    second day- part 2

---
+++ Bachshop-microservices/src/Services/Catalog/CatalogAPI/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,21 @@
+﻿using CatalogAPI.Products.GetProductById;
+
+namespace CatalogAPI.ProGetProductByIdResponseducts.GetProductById
+{
+    public record GetProductByIdQuery(Guid Id):IQuery<GetProductByIdResult>;
+    public record GetProductByIdResult(Product Product);
+    public class GetProductByIdQueryHandler(IDocumentSession session,ILogger<GetProductByIdQueryHandler> logger)
+        : IQueryHandler<GetProductByIdQuery, GetProductByIdResult>
+    {
+        public async Task<GetProductByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductsQueryHandler.Handle called with {@Query}", query);
+            var product= await session.LoadAsync<Product>(query.Id,cancellationToken);
+            if(product is null)
+            {
+                throw new ProductNotFoundException();
+            }
+            return new GetProductByIdResult(product);
+        }
+    }
+}

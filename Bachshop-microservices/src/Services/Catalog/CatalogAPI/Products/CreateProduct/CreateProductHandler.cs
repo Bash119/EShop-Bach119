6246ba02f25e490a6a55a3558d541b6f5bd71fde
Bashar Saab <bashar.saab@gmail.com>
2024-11-14 13:07:47 -0500

    Third day-1

--- Bachshop-microservices/src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
+++ Bachshop-microservices/src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -1,8 +1,8 @@
 ﻿
 
-using BuildingBlocks.CQRS;
-using CatalogAPI.Models;
-using Marten;
+
+
+using JasperFx.CodeGeneration.Frames;
 
 namespace CatalogAPI.Products.CreateProduct
 {
@@ -11,11 +11,30 @@ namespace CatalogAPI.Products.CreateProduct
         : ICommand<CreateProductResult>;
 
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler(IDocumentSession session)
+
+    public class CreateProductCommandValidator : AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductCommandValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Name is rerquired");
+            RuleFor(x => x.Category).NotEmpty().WithMessage("Category is required");
+            //RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required");
+            RuleFor(x => x.ImageFile).NotEmpty().WithMessage("Image is required");
+            RuleFor(x => x.Price).GreaterThan(0).WithMessage("Price should be greater than 0");
+        }
+    }
+
+    internal class CreateProductCommandHandler(IDocumentSession session,IValidator<CreateProductCommand> validator)
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
+            var result =await validator.ValidateAsync(command, cancellationToken);
+            var errors=result.Errors.Select(x => x.ErrorMessage).ToList();
+            if(errors.Any())
+            {
+                throw new ValidationException(errors.FirstOrDefault());
+            }
             //create product entity from command object
             var product = new Product
             {
--- Bachshop-microservices/src/Services/Catalog/CatalogAPI/Program.cs
+++ Bachshop-microservices/src/Services/Catalog/CatalogAPI/Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddMarten(opts =>
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
 
+builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+
 var app = builder.Build();
 
 
